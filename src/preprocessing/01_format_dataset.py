371cdfd7d2c64c520cb5fe77e683bdb731859a10
thaoanhlehoang <thaoanhlee06@gmail.com>
2026-03-20 11:48:02 +0700

    dataset

--- src/preprocessing/01_format_dataset.py
+++ src/preprocessing/01_format_dataset.py
@@ -16,22 +16,27 @@ queries_in = "dataset/raw/tydiqa-goldp-vietnamese/queries.jsonl"
 #UIT-ViQuAD2.0
 uit_in = ["dataset/raw/UIT-ViQuAD2.0/train.jsonl", "dataset/raw/UIT-ViQuAD2.0/validation.jsonl"]
 
-output = "dataset/preprocessed/processed_data.jsonl"
+mmarco_out = "dataset/preprocessed/pr_mmarco.jsonl"
+goldp_out = "dataset/preprocessed/pr_goldp.jsonl"
+uit_out = "dataset/preprocessed/pr_uit.jsonl"
+
+# output = "dataset/preprocessed/processed_data.jsonl"
 
 if __name__ == "__main__":
-    with open(output, "w", encoding="utf-8") as f_out:
-        with open (mmarco_in, "r", encoding="utf-8") as f_in:
-            for line in f_in:
-                item = json.loads(line)
-                query = item["query"]
-                answer = item["positive"]
-                if len(answer) < 200:
-                    continue
-                if detect(answer) == 'en':
-                    continue
-                f_out.write(json.dumps({"query": query, "answer": answer}, ensure_ascii=False) + "\n")
 
-        corpus_dict = {}
+    with open (mmarco_in, "r", encoding="utf-8") as f_in, open(mmarco_out, "w", encoding="utf-8") as f_out:
+        for line in f_in:
+            item = json.loads(line)
+            query = item["query"]
+            answer = item["positive"]
+            if len(answer) < 200:
+                continue
+            if detect(answer) == 'en':
+                continue
+            f_out.write(json.dumps({"query": query, "answer": answer}, ensure_ascii=False) + "\n")
+
+    corpus_dict = {}
+    with open(goldp_out, "w", encoding="utf-8") as f_out:
         with open(corpuses_in, "r", encoding="utf-8") as f:
             for line in f:
                 item = json.loads(line)
@@ -47,7 +52,8 @@ if __name__ == "__main__":
                     continue
                 if answer:
                     f_out.write(json.dumps({"query":query, "answer": answer}, ensure_ascii=False)+ "\n")
-        
+    
+    with open(uit_out, "w", encoding="utf-8") as f_out:
         for input_file in uit_in:
             with open(input_file, "r", encoding="utf-8") as f_in:
                 for line in f_in:
